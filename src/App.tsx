65b824bd52f5dc1f9214abcfb5156925e1f5a023
Teodor Raychev <teodor.raychev@concentrix.com>
2023-10-23 15:42:31 +0300

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 //import hole from "./assets/hole.png"
-import { Mole } from './components/Mole';
+import { Mole } from "./components/Mole";
 
 const App: React.FC = () => {
-  const [score, setScore] = useState (0);
+  const [score, setScore] = useState(0);
   const [moleVisible, setMoleVisible] = useState(false);
-  const [molePosition, setMolePosition] = useState({row: 0, col:0});
+  const [molePosition, setMolePosition] = useState({ row: 0, col: 0 });
   const [startGame, setStartGame] = useState(false);
   const [moleDuration, setMoleDuration] = useState(1000); //in milliseconds
   const [successfulClicks, setSuccessfulClicks] = useState(0);
@@ -14,26 +14,27 @@ const App: React.FC = () => {
   const [totalClicks, setTotalClicks] = useState(0);
   const [successRate, setSuccessRate] = useState(100);
   const durationUntilNextMole = 1500;
- // const gridCellRefs = Array.from({ length: 16 }, () => useRef(null));
-
+  // const gridCellRefs = Array.from({ length: 16 }, () => useRef(null));
 
   const handleMoleClick = (row: number, col: number) => {
-    if(moleVisible && row === molePosition.row && col === molePosition.col) {
-      setScore(score + 1 );
+    if (moleVisible && row === molePosition.row && col === molePosition.col) {
+      setScore(score + 1);
       setSuccessfulClicks((SuccessfulClicks) => SuccessfulClicks + 1);
       setMoleVisible(false);
       setMoleDuration((MoleDuration) => Math.max(50, MoleDuration - 50));
       setTotalClicks(totalClicks + 1);
-    }else {
+    } else {
       setMissedClicks((MissedClicks) => MissedClicks + 1);
-      setMoleDuration((MoleDuration) => Math.min(durationUntilNextMole, MoleDuration + 100));
+      setMoleDuration((MoleDuration) =>
+        Math.min(durationUntilNextMole, MoleDuration + 100)
+      );
       setTotalClicks(totalClicks + 1);
     }
   };
-  const rate = totalClicks > 0 ? (successfulClicks/(totalClicks))*100 : 0;
-  useEffect(()=>{
+  const rate = totalClicks > 0 ? (successfulClicks / totalClicks) * 100 : 0;
+  useEffect(() => {
     setSuccessRate(rate);
-  },[rate]);
+  }, [rate]);
 
   const handleResetScore = () => {
     setScore(0);
@@ -49,60 +50,69 @@ const App: React.FC = () => {
       setStartGame(false);
       setMoleVisible(false);
     } else {
-    setStartGame(true);
+      setStartGame(true);
     }
   };
 
   useEffect(() => {
-    let moleInterval: NodeJS.Timeout | null = null; 
-    if(startGame){
-    moleInterval = setInterval(()=> {
-      const row = Math.floor(Math.random() * 4);
-      const col = Math.floor(Math.random() * 4);
-      setMolePosition({row,col});
-      setMoleVisible(true);
-      setTimeout(() => {
-        setMoleVisible(false);
-      },moleDuration);
-    }, durationUntilNextMole);
-  }
+    let moleInterval: NodeJS.Timeout | null = null;
+    if (startGame) {
+      moleInterval = setInterval(() => {
+        const row = Math.floor(Math.random() * 4);
+        const col = Math.floor(Math.random() * 4);
+        setMolePosition({ row, col });
+        setMoleVisible(true);
+        setTimeout(() => {
+          setMoleVisible(false);
+        }, moleDuration);
+      }, durationUntilNextMole);
+    }
     return () => {
-      if(moleInterval) clearInterval(moleInterval);
+      if (moleInterval) clearInterval(moleInterval);
     };
-  }, [startGame,moleDuration]);
+  }, [startGame, moleDuration]);
 
   return (
     <div className="App">
-      <div className='input-box'> 
-      <input 
-        className="game-input"
-        type="number"
-        value={moleDuration}
-        onChange={(e) => setMoleDuration(parseInt(e.target.value,10))}
-        placeholder="Enter duration in milliseconds"
-      /></div>
-      <button className="game-button" onClick = {handleStartStopGame}>{startGame ? 'Stop' : 'Start'}</button>
-      <button className="game-button" onClick = {handleResetScore}>Reset</button>
+      <div className="input-box">
+        <input
+          className="game-input"
+          type="number"
+          value={moleDuration}
+          onChange={(e) => setMoleDuration(parseInt(e.target.value, 10))}
+          placeholder="Enter duration in milliseconds"
+        />
+      </div>
+      <button className="game-button" onClick={handleStartStopGame}>
+        {startGame ? "Stop" : "Start"}
+      </button>
+      <button className="game-button" onClick={handleResetScore}>
+        Reset
+      </button>
       <div className="score">
         <div>Score: {score} </div>
         <div>Successful Clicks: {successfulClicks}</div>
-        <div>Missed Clicks: {missedClicks}</div>  
-        <div>Total Clicks: {totalClicks}</div>  
-        <div>Success Rate: {successRate.toFixed(0)}%</div>      
+        <div>Missed Clicks: {missedClicks}</div>
+        <div>Total Clicks: {totalClicks}</div>
+        <div>Success Rate: {successRate.toFixed(0)}%</div>
       </div>
       <div className="grid">
         {[0, 1, 2, 3].map((row) =>
           [0, 1, 2, 3].map((col) => (
             <Mole
               key={`${row}-${col}`}
-              visible={moleVisible && row === molePosition.row && col === molePosition.col}
-              onClick = {() => handleMoleClick(row, col)}
+              visible={
+                moleVisible &&
+                row === molePosition.row &&
+                col === molePosition.col
+              }
+              onClick={() => handleMoleClick(row, col)}
             />
           ))
         )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Mole.tsx
+++ src/components/Mole.tsx
@@ -1,32 +1,33 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 
-export const Mole: React.FC<{ visible: boolean; onClick: () => void; }>= ({
-        visible,
-        onClick, }) => {
-            const gridCellRef = React.createRef<HTMLDivElement>();
+export const Mole: React.FC<{ visible: boolean; onClick: () => void }> = ({
+  visible,
+  onClick,
+}) => {
+  const gridCellRef = React.createRef<HTMLDivElement>();
 
-            const applyClassAndRemove =(className: string) => {
-                gridCellRef.current?.classList.add(className);
-                setTimeout(()=> {
-                    gridCellRef.current?.classList.remove(className);
-                },250);
-            };
-            useEffect(()=>{
-                return()=> {
-                    if(gridCellRef.current){
-                        gridCellRef.current.classList.remove('successful','missed');
-                    }
-                };
-            },[]);
-        return (
-        <div 
-            className={`mole ${visible ? 'visible': '' }`}
-            ref={gridCellRef}
-            onClick ={() => {
-                onClick();
-                if (visible) applyClassAndRemove('successful');
-                else applyClassAndRemove('missed');
-            }}>
-        </div>
-    );
+  const applyClassAndRemove = (className: string) => {
+    gridCellRef.current?.classList.add(className);
+    setTimeout(() => {
+      gridCellRef.current?.classList.remove(className);
+    }, 250);
+  };
+  useEffect(() => {
+    return () => {
+      if (gridCellRef.current) {
+        gridCellRef.current.classList.remove("successful", "missed");
+      }
+    };
+  }, []);
+  return (
+    <div
+      className={`mole ${visible ? "visible" : ""}`}
+      ref={gridCellRef}
+      onClick={() => {
+        onClick();
+        if (visible) applyClassAndRemove("successful");
+        else applyClassAndRemove("missed");
+      }}
+    ></div>
+  );
 };
